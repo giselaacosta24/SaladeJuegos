b02438d9ceac86f4b4f065f7f755b1147d0500b8
giselaacosta24 <giseacosta651@gmail.com>
2022-04-06 21:03:22 -0300

    heroku

--- salajuegos/src/environments/environment.prod.ts
+++ salajuegos/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyA_rodTSFm1QEoW2Bw9udvfy1c_r6HbX38",
+    authDomain: "saladejuegos-43468.firebaseapp.com",
+    projectId: "saladejuegos-43468",
+    storageBucket: "saladejuegos-43468.appspot.com",
+    messagingSenderId: "458668032808",
+    appId: "1:458668032808:web:7c7ffbfb04c37676f1061d",
+    measurementId: "G-0Z5V5ZBP5K"
+  }
 };
